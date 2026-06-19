78312b5fbbfb50601ef0796afebd426f303377b0
Manohar <manoharvemula2006@gmail.com>
2026-06-19 18:03:01 +0530

    Loopings had done

--- looping.py
+++ looping.py
@@ -55,3 +55,8 @@ for i in range(1,6):
     for j in range(i):
         print("*",end=" ")
     print()
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(1, rows + 1):
+    print((" " * (rows - i)) + ("*" * (2 * i - 1)))
