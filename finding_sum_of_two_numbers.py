f7a7ea640a3485f85349e7a1cbcd69623227d64e
Manohar <manoharvemula2006@gmail.com>
2026-07-20 10:53:47 +0530

    finding_sum_of_two_numbers

--- finding_sum_of_two_numbers.py
+++ finding_sum_of_two_numbers.py
@@ -2,3 +2,14 @@ num1=int(input("Enter first number:"))
 num2=int(input("Enter second number:"))
 
 print(f"Addition of {num1} and {num2} is {num1 + num2}")
+
+# Original
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(f"Sum is {num1 + num2}")
+
+# Modify for 3 numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+print(f"Sum is {num1 + num2 + num3}")
